67ecb13fe5dc828cb791b433fae6e52b5b26a63f
Eugeniu Ciolacu <eugenciolacu@gmail.com>
2021-12-08 17:15:58 +0200

    Started Proxy example

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -6,7 +6,12 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object. " +
+                "A proxy controls access to the original object, allowing you to perform something either before or after the request gets through to " +
+                "the original object.");
+            Console.WriteLine();
+
+
         }
     }
 }
