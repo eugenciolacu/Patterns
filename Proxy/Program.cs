9267a43c8ce976ea21e1d02b4410f17308d7bc7c
Eugeniu Ciolacu <eugenciolacu@gmail.com>
2021-12-09 11:41:26 +0200

    Added Proxy example

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -11,7 +11,17 @@ namespace Proxy
                 "the original object.");
             Console.WriteLine();
 
+            Console.WriteLine("Client passing employee with Role Developer to folderproxy");
+            Employee emp1 = new Employee("Eugen", "1234", "Developer");
+            SharedFolderProxy folderProxy1 = new SharedFolderProxy(emp1);
+            folderProxy1.PerformRWOperations();
+            
+            Console.WriteLine();
 
+            Console.WriteLine("Client passing employee with Role Manager to folderproxy");
+            Employee emp2 = new Employee("Mihai", "1234", "Manager");
+            SharedFolderProxy folderProxy2 = new SharedFolderProxy(emp2);
+            folderProxy2.PerformRWOperations();
         }
     }
 }
